429dfd2a743439531ddcf4141faac361b577cce4
Sjoerd Hoeven <sjoerdh.sh@gmail.com>
2026-01-09 13:21:57 +0100

    config updatea

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/<YOUR_REPO_NAME>/', // TODO: Replace <YOUR_REPO_NAME> with your actual GitHub repository name
+  base: '/frontendtest/',
 })
